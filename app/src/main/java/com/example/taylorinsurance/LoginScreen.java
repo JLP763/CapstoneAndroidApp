61ebc0380e4d1b471fc3d7a9b0ea27f06e166f93
Jenn <jennpower_14@icloud.com>
2021-03-27 17:13:24 -0230

    Designed and coded log in screen. Main Activity set up, but needs to be customized to Insurance info.

--- app/src/main/java/com/example/taylorinsurance/LoginScreen.java
+++ app/src/main/java/com/example/taylorinsurance/LoginScreen.java
@@ -2,13 +2,47 @@ package com.example.taylorinsurance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginScreen extends AppCompatActivity {
+
+    private EditText Username;
+    private EditText Password;
+    private Button Login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        Username = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        //Login
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authenticate(Username.getText().toString(),Password.getText().toString());
+            }
+        });
+
+    }
+
+    //Function for authenticating login
+    private void authenticate(String username, String password){
+        if ((username.equals("P3N")) && (password.equals("P3N"))){
+            Intent intent = new Intent(LoginScreen.this, MainActivity.class);
+            startActivity(intent);
+        } else{
+            Toast toast = Toast.makeText(LoginScreen.this, "Username and Password cannot be null.",Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
