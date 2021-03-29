da080bf489f9886f882293ff00bcaefbc1657e21
Jenn <jennpower_14@icloud.com>
2021-03-29 19:10:54 -0230

    Navigation Menu updated with Taylor Insurance Theme

--- app/src/main/java/com/example/taylorinsurance/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/example/taylorinsurance/ui/gallery/GalleryViewModel.java
@@ -10,7 +10,7 @@ public class GalleryViewModel extends ViewModel {
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is home insurance tips fragment");
     }
 
     public LiveData<String> getText() {
